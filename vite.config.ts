ed002d7900549d093c25751470b0c70557579f79
Arif Rachim <a.arif.r@gmail.com>
2024-04-20 13:36:58 +0400

    build(vite.config.ts): fix the coverage format vite.config to use json instead of default

--- vite.config.ts
+++ vite.config.ts
@@ -26,5 +26,11 @@ export default defineConfig({
   test : {
     globals:true,
     environment: 'jsdom',
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    }
   },
 })
