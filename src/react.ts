f6d57439f549a99a1caebb58595da3addf479072
Alfin S <benjaminstwo@gmail.com>
2024-04-21 09:04:25 +0800

    docs(jsdoc): useSignal hook usage example and information

--- src/react.ts
+++ src/react.ts
@@ -3,6 +3,28 @@ import React from "react";
 import { effect } from "./effect.ts";
 import { type WithSignal, snapshot } from "./signal.ts";
 
+/**
+ * useSignal
+ *
+ * Create a local snapshot from signal that catches signal.
+ * The component will only re-render when the parts of the state you access have changed, it is render-optimized.
+ *
+ * @example
+ * ```js
+ * const counter = signal(0);
+
+ * function Counter() {
+ *    const snap = useSignal(counter);
+ *    return (
+ *      <div>
+ *          {snap}
+ *          <button onClick={() => ++counter.value}>+1</button>
+ *      </div>
+ *    )
+ * }
+ * ```
+ *
+ */
 export function useSignal<T>(target: WithSignal<T>): T {
   const subscribe = React.useCallback(function subscribe(callback: () => void) {
     return effect(callback);
