1d13a8f2fbfbdc80c0f770a01dd81be50dfd020e
Alfin S <benjaminstwo@gmail.com>
2024-04-21 12:38:01 +0800

    chore(root): prefer use index as a root lib

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export { useSignal } from "./react";
+export {
+  signal,
+  isSignal,
+  toValue,
+  type WithSignal,
+  type ReadonlySignal,
+  type MaybeSignal,
+} from "./signal.ts";
+export {
+  computed,
+  type ComputedSignal,
+  type WritableComputedSignal,
+  type ComputedGetter,
+  type ComputedSetter,
+  type WritableComputedOptions,
+} from "./computed.ts";
--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,36 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 /// https://vitejs.dev/config/
 
 export default defineConfig({
-  plugins: [dts({rollupTypes:true,outDir:'./dist'})],
+  plugins: [dts({ rollupTypes: true, outDir: "./dist" })],
   build: {
     lib: {
-      entry: './src/main.ts',
-      name: 'ReactHookSignal',
-      fileName: 'react-hook-signal',
+      entry: "./src/index.ts",
+      name: "ReactHookSignal",
+      fileName: "react-hook-signal",
     },
-    minify:false,
+    minify: false,
     rollupOptions: {
       // Optional: Configure Rollup for library building
-      external: ['react', 'react-dom','signal-polyfill'], // Externalize React dependencies
+      external: ["react", "react-dom", "signal-polyfill"], // Externalize React dependencies
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+          react: "React",
+          "react-dom": "ReactDOM",
         },
       },
     },
   },
-  test : {
-    globals:true,
-    environment: 'jsdom',
+  test: {
+    globals: true,
+    environment: "jsdom",
     coverage: {
       // you can include other reporters, but 'json-summary' is required, json is recommended
-      reporter: ['text', 'json-summary', 'json'],
+      reporter: ["text", "json-summary", "json"],
       // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
       reportOnFailure: true,
-    }
+    },
   },
-})
+});
