34464c8faeede65a07ac68d89039df6ed83552e3
Alfin S <benjaminstwo@gmail.com>
2024-04-21 09:05:54 +0800

    chore: shared computed to public

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,4 @@ export { useSignalEffect, useSignal, type JSXAttribute } from "./hooks.ts";
 // New RFCs API
 export { useSignal as useSignalExperimental } from "./react";
 export { signal, type WithSignal } from "./signal.ts";
+export { computed } from "./computed.ts";
