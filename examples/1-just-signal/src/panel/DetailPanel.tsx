d1b7cfae47b883bc28311d35544f3ccde2f65abe
unknown <a.arif.r@gmail.com>
2024-04-22 20:09:56 +0400

    docs(1-just-signal): continue working on the sample just signal

--- examples/1-just-signal/src/panel/DetailPanel.tsx
+++ examples/1-just-signal/src/panel/DetailPanel.tsx
@@ -1,34 +1,38 @@
-import {Signal} from "signal-polyfill";
 import {Todo} from "../model/Todo.ts";
-import {notifiable} from "../../../../src/main.ts";
+import {notifiable, useSignal} from "../../../../src/main.ts";
 import {formatDateForInputDate} from "../utils/formatDateForInputData.ts";
+import {useComputed} from "../../../../src/hooks.ts";
 
 export function DetailPanel() {
-    const todo = new Signal.State<Partial<Todo>>({});
-    const dueDate = new Signal.Computed(() => formatDateForInputDate(todo.get().dueDate))
-    const priority = new Signal.Computed(() => todo.get().priority);
-    const title = new Signal.Computed(() => todo.get().title ?? '');
-    const description = new Signal.Computed(() => todo.get().description ?? '');
 
-    const userHasTriedToSubmit = new Signal.State(false);
+    const todo = useSignal<Partial<Todo>>({});
+    const dueDate = useComputed(() => formatDateForInputDate(todo.get().dueDate))
+    const priority = useComputed(() => todo.get().priority);
+    const title = useComputed(() => todo.get().title ?? '');
+    const description = useComputed(() => todo.get().description ?? '');
 
-    const dueDateError = new Signal.Computed(() => userHasTriedToSubmit.get() ? isEmpty(todo.get().dueDate) ? 'Due Date required' : '' : '');
-    const priorityError = new Signal.Computed(() => userHasTriedToSubmit.get() ? isEmpty(todo.get().priority) ? 'Priority required' : '' : '');
-    const titleError = new Signal.Computed(() => userHasTriedToSubmit.get() ? isEmpty(todo.get().title) ? 'Title required' : '' : '');
-    const descriptionError = new Signal.Computed(() => userHasTriedToSubmit.get() ? isEmpty(todo.get().description) ? 'Description required' : '' : '');
+    const userHasTriedToSubmit = useSignal(false);
+    //const isEditMode = useSignal(false);
 
-    const dueDateClassName = new Signal.Computed(() => `border-2 rounded-lg grow p-1 pl-2 border-${isEmpty(dueDateError.get())?'gray':'red'}-400`);
-    const priorityClassName = new Signal.Computed(() => `border-2 rounded-lg p-2 border-${isEmpty(priorityError.get())?'gray':'red'}-400`);
-    const titleClassName = new Signal.Computed(() => `border-2 rounded-lg p-2 border-${isEmpty(titleError.get())?'gray':'red'}-400`);
-    const descriptionClassName = new Signal.Computed(() => `border-2 rounded-lg p-2 border-${isEmpty(descriptionError.get())?'gray':'red'}-400 h-32`);
+
+    const dueDateError = useComputed(() => userHasTriedToSubmit.get() ? isEmpty(todo.get().dueDate) ? 'Due Date required' : '' : '');
+    const priorityError = useComputed(() => userHasTriedToSubmit.get() ? isEmpty(todo.get().priority) ? 'Priority required' : '' : '');
+    const titleError = useComputed(() => userHasTriedToSubmit.get() ? isEmpty(todo.get().title) ? 'Title required' : '' : '');
+    const descriptionError = useComputed(() => userHasTriedToSubmit.get() ? isEmpty(todo.get().description) ? 'Description required' : '' : '');
+
+    const dueDateClassName = useComputed(() => `border-2 rounded grow p-1 pl-2 border-${isEmpty(dueDateError.get())?'gray':'red'}-400`);
+    const priorityClassName = useComputed(() => `border-2 rounded p-2 border-${isEmpty(priorityError.get())?'gray':'red'}-400`);
+    const titleClassName = useComputed(() => `border-2 rounded p-2 border-${isEmpty(titleError.get())?'gray':'red'}-400`);
+    const descriptionClassName = useComputed(() => `border-2 rounded p-2 border-${isEmpty(descriptionError.get())?'gray':'red'}-400 h-32`);
 
     return (
         <>
-            <form className={"flex flex-col"} onSubmit={(event) => {
+            <form className={"flex flex-col gap-5"} onSubmit={(event) => {
                 event.preventDefault();
                 event.stopPropagation();
                 userHasTriedToSubmit.set(true);
             }}>
+                <h1 className={'font-normal text-3xl border-b-2'}>Todo</h1>
                 <div className={'flex flex-row gap-5'}>
                     <label className={'flex flex-col w-1/2'}>
                         Due Date :
@@ -49,9 +53,10 @@ export function DetailPanel() {
                                                priority: e.target.value as Todo["priority"]
                                            })}
                         >
-                            <option>High</option>
-                            <option>Medium</option>
-                            <option>Low</option>
+                            <option value={''}></option>
+                            <option value={'High'}>High</option>
+                            <option value={'Medium'}>Medium</option>
+                            <option value={'Low'}>Low</option>
                         </notifiable.select>
                         <notifiable.div className={'flex flex-col items-end text-red-600'}>
                             {priorityError}
@@ -74,15 +79,22 @@ export function DetailPanel() {
                                          value={description}
                                          onChange={e => todo.set({...todo.get(), description: e.target.value})}
                     />
+
                     <notifiable.div className={'flex flex-col items-end text-red-600'}>
                         {descriptionError}
                     </notifiable.div>
                 </label>
+                <div className={'flex flex-row justify-end gap-4'}>
+                    <button className={'flex p-3 bg-gray-200 rounded w-20 justify-center font-medium'}>Edit</button>
+                    <button className={'flex p-3 bg-gray-200 rounded w-20 justify-center font-medium'}>Save</button>
+                </div>
             </form>
         </>
     )
 }
 
 function isEmpty(value: unknown) {
-    return value === undefined || value === null || value === '' || value.toString().trim() === '';
+    const empty = value === undefined || value === null || value === '' || value.toString().trim() === '';
+    console.log('is empty',value,empty);
+    return empty;
 }
