e80ff25455f8b1d329037dbb226ea491da84dc5e
arif-rachim <a.arif.r@gmail.com>
2024-09-06 15:55:30 +0400

    Add filtering and sorting to TableEditor
    Enhanced TableEditor to support filtering and sorting functionalities. Added new props and updated query logic to apply filters and sorting. Updated UI to reflect these changes with input fields for filters and sortable headers.

--- examples/app-craft/src/app-designer/panels/database/table-editor/TableEditor.tsx
+++ examples/app-craft/src/app-designer/panels/database/table-editor/TableEditor.tsx
@@ -11,6 +11,8 @@ import {Editor} from "@monaco-editor/react";
 import {isEmpty} from "../../../../utils/isEmpty.ts";
 import {useAppContext} from "../../../hooks/useAppContext.ts";
 import {PageViewer} from "../../../../app-viewer/PageViewer.tsx";
+import {QueryTypeResult} from "../../../query-grid/QueryGrid.tsx";
+import {MdArrowDownward, MdArrowUpward} from "react-icons/md";
 
 
 export async function queryPagination(query: string, params: BindParams, currentPage: number, pageSize: number) {
@@ -34,29 +36,51 @@ export async function queryPagination(query: string, params: BindParams, current
     })
 }
 
-async function queryTable(table: Table, currentPage: number, setTableData: Dispatch<SetStateAction<{
-    columns: string[];
-    data: unknown[];
-    currentPage: number;
-    totalPage: number
-}>>) {
-    const result = await queryPagination(`select * from ${table.tblName}`, [], currentPage, 50);
-    setTableData(result);
+async function queryTable(props: {
+    table: Table,
+    currentPage: number,
+    setTableData: Dispatch<SetStateAction<QueryTypeResult>>,
+    filter: Record<string, string>,
+    sort :Array<{ column: string, direction: 'asc' | 'desc' }>
+}) {
+    const {setTableData, table, currentPage, filter,sort} = props;
+
+    const paramsString: string[] = [];
+    Object.keys(filter).forEach(key => {
+        paramsString.push(`${key} LIKE '%${filter[key]}%'`);
+    });
+    const sortStrings:string[] = [];
+    sort.forEach(s => {
+        sortStrings.push(`${s.column} ${s.direction}`);
+    })
+    const result = await queryPagination(`SELECT * FROM ${table.tblName} ${paramsString.length > 0 ?' WHERE ':''} ${paramsString.join(' AND ')} ${sortStrings.length > 0 ? 'ORDER BY':''} ${sortStrings.join(', ')}`, [], currentPage, 50);
+    setTableData(oldValue => {
+        if (result.data.length > 0) {
+            return result;
+        }
+        if (oldValue.columns && oldValue.columns.length > 0) {
+            return {...oldValue, data: [], currentPage: 0, totalPage: 0}
+        }
+        return oldValue;
+    });
 }
 
 export default function TableEditor(props: { table: Table }) {
     const {table} = props;
-    const [tableData, setTableData] = useState<{
-        columns: string[],
-        data: unknown[],
-        currentPage: number,
-        totalPage: number
-    }>({columns: [], data: [], currentPage: 0, totalPage: 0});
+    const [tableData, setTableData] = useState<QueryTypeResult>({columns: [], data: [], currentPage: 0, totalPage: 0});
+    const [filter, setFilter] = useState<Record<string, string>>({})
+    const [sort,setSort] = useState<Array<{ column: string, direction: 'asc' | 'desc' }>>([])
     useEffect(() => {
         (async () => {
-            await queryTable(table, 1, setTableData);
+            await queryTable({
+                table,
+                currentPage: 1,
+                setTableData,
+                filter,
+                sort
+            });
         })();
-    }, [table]);
+    }, [table, filter,sort]);
     const [isOpen, setOpen] = useState(false);
     return <div
         style={{display: 'flex', flexDirection: 'column', overflow: 'auto', height: '100%'}}>
@@ -74,12 +98,47 @@ export default function TableEditor(props: { table: Table }) {
             </div>
         </CollapsibleLabelContainer>
         <div style={{flexGrow: 1, overflow: 'auto', display: 'flex', flexDirection: 'column'}}>
-            <SimpleTable columns={tableData.columns} data={tableData.data as Array<Record<string, unknown>>}
-                         keyField={'id'}/>
+            <SimpleTable columns={tableData.columns ?? []} data={tableData.data as Array<Record<string, unknown>>}
+                         keyField={'ID_'} filterable={true} filter={filter}
+                         onFilterChange={({column, value}) => {
+                             setFilter(oldValue => {
+                                 const returnValue = {...oldValue};
+                                 if (typeof value === 'string') {
+                                     returnValue[column] = value;
+                                 } else {
+                                     returnValue[column] = JSON.stringify(value);
+                                 }
+                                 return returnValue
+                             })
+                         }}
+                         sortable={true}
+                         sort={sort}
+                         onSortChange={({column,value}) => {
+                             setSort(oldValue => {
+                                 const newValue = [...oldValue];
+                                 if(value === 'remove'){
+                                     return newValue.filter(c => c.column !== column)
+                                 }
+                                 const itemIndex = newValue.findIndex(c => c.column === column);
+                                 if(itemIndex < 0) {
+                                     newValue.push({column, direction: value});
+                                 }else{
+                                     newValue.splice(itemIndex,1,{column: column, direction: value});
+                                 }
+                                 return newValue;
+                             })
+                         }}
+            />
         </div>
         <SimpleTableFooter value={tableData?.currentPage ?? 0} totalPages={tableData?.totalPage ?? 1}
                            onChange={async (page) => {
-                               await queryTable(table, page, setTableData);
+                               await queryTable({
+                                   table,
+                                   currentPage: page,
+                                   setTableData,
+                                   filter,
+                                   sort
+                               });
                            }}/>
     </div>
 }
@@ -160,13 +219,36 @@ export type ColumnsConfig = Record<string, {
 }>
 
 
+function extractWidthAndHiddenField(columnsConfig: ColumnsConfig | undefined, col: string) {
+    let width: CSSProperties['width'] | undefined = undefined;
+    let hide: boolean | undefined = false;
+    if (columnsConfig !== undefined && columnsConfig !== null && typeof columnsConfig === 'object' && col in columnsConfig) {
+        const config = columnsConfig[col];
+        if (!isEmpty(config.width)) {
+            {
+                width = config.width;
+            }
+        }
+        if (config.hidden !== undefined) {
+            hide = config.hidden;
+        }
+    }
+    return {width, hide};
+}
+
 export function SimpleTable<T extends Record<string, unknown>>(props: {
     columns: Array<string>,
     data: Array<T>,
     keyField: string,
     onFocusedRowChange?: (focusedItem: T) => void,
     focusedRow?: T,
-    columnsConfig?: ColumnsConfig
+    columnsConfig?: ColumnsConfig,
+    filterable?: boolean,
+    filter?: Record<string, unknown>,
+    onFilterChange?: (props: { column: string, value: unknown, oldValue: unknown }) => void,
+    sortable?: boolean,
+    sort?: Array<{ column: string, direction: 'asc' | 'desc' }>,
+    onSortChange?: (props: { column: string, value: 'asc' | 'desc' | 'remove' }) => void
 }) {
     const {
         columns,
@@ -174,7 +256,13 @@ export function SimpleTable<T extends Record<string, unknown>>(props: {
         keyField,
         focusedRow: focusedRowProps,
         onFocusedRowChange,
-        columnsConfig
+        columnsConfig,
+        filterable,
+        filter,
+        onFilterChange,
+        sort,
+        onSortChange,
+        sortable
     } = props;
 
     const [focusedRow, setFocusedRow] = useState<T | undefined>(focusedRowProps);
@@ -185,21 +273,20 @@ export function SimpleTable<T extends Record<string, unknown>>(props: {
     return <div style={{display: 'table', maxHeight: '100%', overflowY: 'auto', overflowX: 'hidden'}}>
         <div style={{display: 'table-row', position: 'sticky', top: 0}}>
             {columns.map(col => {
-                let width: CSSProperties['width'] | undefined = undefined;
-                let hide: boolean | undefined = false;
+                const {width, hide} = extractWidthAndHiddenField(columnsConfig, col);
                 let title = col;
                 if (columnsConfig !== undefined && columnsConfig !== null && typeof columnsConfig === 'object' && col in columnsConfig) {
                     const config = columnsConfig[col];
-                    if (!isEmpty(config.width)) {
-                        {
-                            width = config.width;
-                        }
-                    }
-                    if (config.hidden !== undefined) {
-                        hide = config.hidden;
-                    }
                     if (config.title) {
                         title = config.title;
+                    }
+                }
+                let sortDirection: 'asc' | 'desc' | undefined = undefined;
+                let sortIndex = -1;
+                if (sortable && sort) {
+                    sortIndex = sort.findIndex(s => s.column === col);
+                    if (sortIndex >= 0) {
+                        sortDirection = sort[sortIndex].direction;
                     }
                 }
                 return <div style={{
@@ -207,11 +294,79 @@ export function SimpleTable<T extends Record<string, unknown>>(props: {
                     borderBottom: BORDER,
                     backgroundColor: '#F2F2F2',
                     color: "black",
-                    padding: '5px 10px',
+                    padding: '5px 0px 5px 10px',
                     width,
-                }}>{title}</div>
+                }} onClick={() => {
+                    if (onSortChange === undefined) {
+                        return;
+                    }
+                    if (sortDirection === 'asc') {
+                        onSortChange({value: 'desc', column: col});
+                    } else if (sortDirection === 'desc') {
+                        onSortChange({value: 'remove', column: col});
+                    } else {
+                        onSortChange({value: 'asc', column: col});
+                    }
+                }}>
+                    <div style={{display: 'flex', justifyContent: 'center', gap: 5}}>
+                        <div style={{width: '100%'}}>
+                            {title}
+                        </div>
+                        {sortable && sortIndex >=0 &&
+                            <div>{(sortIndex + 1).toString()}</div>
+                        }
+                        {sortable &&
+                            <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                                {sortDirection === 'asc' &&
+                                    <MdArrowUpward/>
+                                }
+                                {sortDirection === 'desc' &&
+                                    <MdArrowDownward/>
+                                }
+                            </div>
+                        }
+                    </div>
+                </div>
             })}
         </div>
+        {filterable &&
+            <div style={{display: 'table-row', position: 'sticky', top: 32}}>
+                {columns.map((col, index, source) => {
+                    const lastIndex = (source.length - 1) === index
+                    const {width, hide} = extractWidthAndHiddenField(columnsConfig, col);
+                    let value = '';
+                    if (filter && col in filter) {
+                        const val = filter[col];
+                        if (typeof val === 'string') {
+                            value = val;
+                        } else {
+                            value = JSON.stringify(val ?? '');
+                        }
+                    }
+                    return <div style={{
+                        display: hide ? 'none' : 'table-cell',
+                        borderBottom: BORDER,
+                        backgroundColor: '#F2F2F2',
+                        color: "black",
+                        width,
+                    }}><input style={{
+                        border: 'unset',
+                        borderRight: lastIndex ? 'unset' : BORDER,
+                        width: '100%',
+                        padding: '5px 10px'
+                    }} value={value} onChange={(e) => {
+                        const val = e.target.value;
+                        if (onFilterChange) {
+                            onFilterChange({
+                                column: col,
+                                value: val,
+                                oldValue: value
+                            });
+                        }
+                    }}/></div>
+                })}
+            </div>
+        }
         {data.map((item, rowIndex) => {
             const key = keyField in item ? item[keyField] : rowIndex;
             const isFocused = item === focusedRow;
